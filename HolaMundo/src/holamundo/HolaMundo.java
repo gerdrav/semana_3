45de1dcfe9b3dda2de7fe586b6d0e8cb2e4ddb8b
gerdrav <gct.draven@gmail.com>
2016-07-24 22:34:52 -0500

    Add files via upload

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,30 @@
+package holamundo;
+
+import java.time.Clock;
+import javax.swing.text.html.parser.DTDConstants;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+        int a=4;
+        int b=2;
+        
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        
+        
+        int i = 2;
+        i++;
+        System.out.println(i);
+        ++i;
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i++);
+        System.out.println(i);
+        
+        
+        
+    }
+}
